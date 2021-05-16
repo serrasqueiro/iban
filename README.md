a9e14249fe73fc9e66ae907b0267cb3ceb639b9e
Henrique Moreira <h@serrasqueiro.com>
2021-05-16 14:11:19 +0100

    Basic pt usage

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
 # iban
 
-IBAN on SEPA
+IBAN on SEPA:
+- git sources:
+  + https://github.com/serrasqueiro/iban/
 
 ## Sources
 
-- `sources/iban-pt.txt`
-  + Generated from [anchor_iban_pt_nib] [1]
+- `sources/iban-pt.txt` - Portugal
+  + Generated from [anchor_iban_pt_nib] ([1])
+- `sources/iban-de.txt` - Deutschland (Germany)
+  + See also [www.iban.com/country/germany/](https://www.iban.com/country/germany/)
+
+- `eea/`
+  + eea ([European Economic Area](https://en.wikipedia.org/wiki/European_Economic_Area)) submodule is not public; it holds [LibreOffice] ([2]) xlsx files with country NIBs.
+  + This is designated *NIBS.xlsx*
+
+The workflow is as follow:
+1. Banking supervisory authority publishes or updates NIB (**B**anking **I**dentification **N**umber)
+1. eea repository is reliably updated accordingly. *NIBS.xlsx* is updated:
+old or disbanded NIBs are marked with a date on the first column (_Expired_).
+1. _sources/iban-$$.txt_ is updated,
+   + here: https://github.com/serrasqueiro/iban/blob/master/sources/
+   + example: sources/iban-**pt**.txt
 
 # Scripts
 
-- `nibs.py` - lists NIBs (pairs) from LibreOffice (xlsx) file
+- `nibs.py` - lists NIBs (pairs) from [LibreOffice] ([2]) (xlsx) file
   + xlsx file should have 3 columns: 'Expired'; 'Code'; 'Name'
+  + currently this script only handles _**pt**_ Country Code; see also [country_code] ([3])
 
 # References
 
@@ -18,3 +35,13 @@ IBAN on SEPA
 
 * [1] - gist [iban_pt_nib]
   + `git@gist.github.com:ed970d4306d6e824d29a9e9e136be654.git`
+
+[2]: https://www.libreoffice.org/discover/libreoffice/ "LibreOffice"
+
+* [2] - LibreOffice [LibreOffice]
+  + [download here](https://www.libreoffice.org/download/)
+
+[3]: https://www.iban.com/country-codes "country_code"
+
+* [3] - Country Code
+  + ISO standard ([here](https://www.iso.org/iso-3166-country-codes.html)): ISO 3166-1 alpha-2
--- nibs.py
+++ nibs.py
@@ -4,6 +4,9 @@
 """
 Displays NIBs from Excel file
 
+IBAN: International Bank Account Number (eea)
+NIB: Banking Identification Number
+
 Uses 'xcelent', openpyxl wrapper
 """
 
@@ -14,20 +17,66 @@ import openpyxl
 import filing.xcelent as xcelent
 import waxpage.redit as redit
 
+DEF_ENCODING = "ISO-8859-1"
+
+DEFAULT_LIBRE_PT_NIBS = "eea/tables/pt_NIBS.xlsx"
+SOURCE_IBAN_CC = "sources/iban-$$.txt"
+
+SAMPLE_IBAN_DE_UNICEF = "IBAN DE57 3702 0500 0000 3000 00"
+
+NIB_NUM_CC = {
+    "@default": 4,
+    "pt": 4,
+    }
+
 
 def main():
     """ Main script """
     main_test(sys.argv[1:])
 
 def main_test(args):
-    dump_nibs(args[0])
+    default_libre_nibs = DEFAULT_LIBRE_PT_NIBS
+    if not args:
+        param = [default_libre_nibs]
+    else:
+        param = args
+    assert param[0]
+    for cc_letters in ("pt",):
+        nibs = {
+            "0000": "(RESERVED)",
+            }
+        dump_nibs(param[0], cc_letters, nibs)
+        out_name = SOURCE_IBAN_CC.replace("$$", cc_letters)
+        is_ok = write_opt_out(cc_letters, out_name, nibs)
+        if is_ok:
+            print(f"# Written: {out_name}")
+        else:
+            print(f"# Did not write: {out_name}")
 
-def dump_nibs(fname) -> int:
+def write_opt_out(cc_letters:str, out_name:str, nibs:dict) -> bool:
+    num_dig = NIB_NUM_CC.get(cc_letters)
+    if num_dig is None:
+        num_dig = NIB_NUM_CC["@default"]
+    try:
+        out = open(out_name, "wb")
+    except PermissionError:
+        return False
+    for nib in sorted(nibs):
+        if nib.isdigit() and int(nib) <= 0:
+            continue
+        shown = nibs[nib]
+        assert len(nib) == num_dig, f"Mismatched length (expected: {num_dig}): $nib"
+        line = f"{nib}\t{shown}\n"
+        out.write(line.encode(DEF_ENCODING))
+    return True
+
+def dump_nibs(fname:str, sheet_name:str, nibs:dict) -> int:
     """ Input should be an OpenLibre xls(x) file """
+    pattern = sorted(nibs)[0]	# e.g. '0000'
     wbk = openpyxl.load_workbook(fname)
     #booklet = xcelent.dict_from_sheets(wbk)
     libre = xcelent.Xcel(wbk, "nibs")
-    sheet = libre.get_sheet("pt")
+    sheet = libre.get_sheet(sheet_name)
     items = [(row[1].value, row[2].value) for row in sheet.rows if row[0].value is None]
     for num, text in items:
         if text is None:
@@ -40,10 +89,62 @@ def dump_nibs(fname) -> int:
             anum = None
         if anum is None:
             continue
-        print(f"{anum}\t{s_text}")
+        if pattern == '0000':
+            s_num = f"{anum:04}"
+        elif pattern.isdigit():
+            fmt = "{" + ":0" + str(len(pattern)) + "}"
+            s_num = fmt.format(anum)
+        else:
+            fmt = "{" + ":<" + str(len(pattern)) + "}"
+            s_num = fmt.format(anum)
+        astr = f"{s_num}\t{s_text}"
+        print(astr)
+        assert s_num not in nibs, f"Duplicate NIB: {s_num}: {astr}"
+        nibs[s_num] = s_text
+        nibs[s_num] = s_text
     return 0
 
 
+def samples(dump:bool=True) -> dict:
+    """ Dump a couple of IBANs """
+    # pylint: disable=line-too-long
+    cc_letter_list = (
+        "pt",	# Portugal
+        "de",	# Germany
+        )
+    urls = {
+        "pt": {
+            "unicef": (
+                "PT50 0033 0000 5013 1901 229 05",
+                "https://www.unicef.pt/como-ajudar/outras-formas-de-fazer-o-seu-donativo/",
+                ),
+            },
+        "de": {
+            "unicef": (
+                "DE57 3702 0500 0000 3000 00",
+                "https://www.unicef.de/informieren/ueber-uns/faq/wie-lautet-die-vollstaendige-bankverbindung-mit-iban-und-bic-von-unicef-deutschland-/27870",
+            )
+            },
+        }
+    result = {"ccs": cc_letter_list, "urls": urls}
+    if not dump:
+        return result
+    for cc_letters in cc_letter_list:
+        print("=" * 20, cc_letters, "=" * 20)
+        for item, there in urls[cc_letters].items():
+            s_who = f"{item} @{cc_letters}"
+            print(":::", item, there)
+            iban, where = there
+            iban = iban.replace(" ", "")
+            #s_iban = iban[:25-2] + " " + iban[25:]
+            s_iban = iban
+            print(s_iban, s_who)
+            print(" " * 4, "URL reference:",
+                  where.split("//", maxsplit=1)[-1])
+            print("--")
+    return result
+
 # Main script
 if __name__ == "__main__":
+    #samples()
     main()
